324a863580a2f20ecbbb59ebd22255b1fb0f7830
trongnd106 <trong.nd106@gmail.com>
2024-09-30 09:05:49 +0700

    Init project

---
+++ src/main/java/com/hththn/dev/department_manager/DepartmentManagerApplication.java
@@ -0,0 +1,13 @@
+package com.hththn.dev.department_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmentManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartmentManagerApplication.class, args);
+	}
+
+}
