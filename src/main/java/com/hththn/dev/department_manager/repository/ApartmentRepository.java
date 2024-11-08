dc9d3fe6319483b6c0800e5193a2f960b54f8e98
trongnd106 <trong.nd106@gmail.com>
2024-11-08 23:12:47 +0700

    Update Apartment repository with filter support

--- src/main/java/com/hththn/dev/department_manager/repository/ApartmentRepository.java
+++ src/main/java/com/hththn/dev/department_manager/repository/ApartmentRepository.java
@@ -2,8 +2,9 @@ package com.hththn.dev.department_manager.repository;
 
 import com.hththn.dev.department_manager.entity.Apartment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ApartmentRepository extends JpaRepository<Apartment, Long> {
+public interface ApartmentRepository extends JpaRepository<Apartment, Long>, JpaSpecificationExecutor<Apartment> {
 }
