7c5115eca5e38df4c1e2111af1b25754fc72a58e
hungbuile04 <builehunghuy2004@gmail.com>
2024-11-28 08:25:41 +0700

    'create: vehicle entity'

---
+++ src/main/java/com/hththn/dev/department_manager/repository/VehicleRepository.java
@@ -0,0 +1,14 @@
+package com.hththn.dev.department_manager.repository;
+
+import com.hththn.dev.department_manager.entity.UtilityBill;
+import com.hththn.dev.department_manager.entity.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface VehicleRepository extends JpaRepository<Vehicle, Long>, JpaSpecificationExecutor<Vehicle> {
+    List<Vehicle> findAllByApartment_AddressNumber(Long addressNumber);
+}
